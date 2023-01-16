15af68a93541fe8ebbe40ce35672926eea8f2205
= <=>
2023-01-16 13:02:15 -0300

    Concorrência Worker Pool

--- 16-Concorrencia/16.5-Select/select.go
+++ 16-Concorrencia/16.5-Select/select.go
@@ -17,28 +17,29 @@ func main() {
 		No cenário sem select a leitura do 1º canal precisa esperar a leitura 2º canal
 		para que os resultados sejam apresentados.
 
-		O correto é que para cada mensagem 2 tivessemos 4x a mensagem 1, pois o tempo de espera é
-		1/2s contra 2s.
+		O correto é que para cada mensagem 2 tivessemos 2x a mensagem 1, pois o tempo de espera é
+		1/2s contra 1s.
 	*/
 	for i := 0; i < iteracoes; i++ {
 		enviamensagem_canal(canal1, canal2, iteracoes)
 		mensagem1 := <-canal1
 		mensagem2 := <-canal2
-		fmt.Println(mensagem1)
-		fmt.Println(mensagem2)
+		fmt.Println("Sem select: " + mensagem1)
+		fmt.Println("Sem select: " + mensagem2)
 	}
 
 	/*
 		No cenário com select os canais são processados à medida em que possuem
 		valores para serem lidos, permitindo que a leitura ocorra sem bloqueios
 	*/
+	iteracoes = 100000
 
 	for i := 0; i < iteracoes; i++ {
 		select {
 		case mensagem1 := <-canal1:
-			fmt.Println(mensagem1)
+			fmt.Println("Com select: " + mensagem1)
 		case mensagem2 := <-canal2:
-			fmt.Println(mensagem2)
+			fmt.Println("Com select: " + mensagem2)
 		}
 	}
 }
@@ -54,7 +55,7 @@ func enviamensagem_canal(canal1, canal2 chan string, iteracoes int) {
 
 	go func(vezes int) {
 		for i := 0; i < vezes; i++ {
-			time.Sleep(time.Microsecond * 2) //Pausa de 2s
+			time.Sleep(time.Second) //Pausa de 1s
 			canal2 <- "Canal 2"
 		}
 
